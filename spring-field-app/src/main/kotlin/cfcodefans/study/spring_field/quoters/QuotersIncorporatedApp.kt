6ad8dc7064f0fb7b7578a940e315d5b4274ae276
cfcodefans <cf.chenfan@gmail.com>
2023-01-03 20:11:04 +0800

    starting with some examples

---
+++ spring-field-app/src/main/kotlin/cfcodefans/study/spring_field/quoters/QuotersIncorporatedApp.kt
@@ -0,0 +1,104 @@
+package cfcodefans.study.spring_field.quoters
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import java.util.Objects
+import kotlin.random.Random
+
+
+@Configuration
+open class DatabaseLoader {
+    @Bean
+    open fun init(quoterRepo: QuoteRepo): CommandLineRunner {
+        return CommandLineRunner { args ->
+            quoterRepo.save(Quote(quote = "Working with Spring Boot is like pair-programming with the Spring developers."))
+            quoterRepo.save(Quote(quote = "With Boot you deploy everywhere you can find a JVM basically."))
+            quoterRepo.save(Quote(quote = "Spring has come quite a ways in addressing developer enjoyment and "
+                    + "ease of use since the last time I built an application using it."))
+            quoterRepo.save(Quote(quote =
+            "Previous to Spring Boot, I remember XML hell, confusing set up, and " + "many hours of frustration."))
+            quoterRepo.save(Quote(quote = "Spring Boot solves this problem. It gets rid of XML and wires up "
+                    + "common components for me, so I don't have to spend hours scratching my "
+                    + "head just to figure out how it's all pieced together."))
+            quoterRepo.save(Quote(quote = "It embraces " + "convention over configuration, providing an experience on par with "
+                    + "frameworks that excel at early stage development, such as Ruby on " + "Rails."))
+            quoterRepo.save(Quote(quote = "The real benefit of Boot, however, is that it's just Spring. That "
+                    + "means any direction the code takes, regardless of complexity, I know " + "it's a safe bet."))
+            quoterRepo.save(Quote(quote = "I don't worry about my code scaling. Boot allows the "
+                    + "developer to peel back the layers and customize when it's appropriate "
+                    + "while keeping the conventions that just work."))
+            quoterRepo.save(Quote(quote = "So easy it is to switch container in #springboot."))
+            quoterRepo.save(Quote(quote = "Really loving Spring Boot, makes stand alone Spring apps easy."))
+            quoterRepo.save(Quote(quote = "I have two hours today to build an app from scratch. @springboot to the rescue!"))
+            quoterRepo.save(Quote(quote = "@springboot with @springframework is pure productivity! Who said in #java one has "
+                    + "to write double the code than in other langs? #FavLib"))
+        }
+    }
+}
+
+@Entity
+open class Quote(@Id @GeneratedValue open var id: Long? = null,
+                 open var quote: String? = null) {
+
+    override fun equals(other: Any?): Boolean = other is Quote
+            && other.id == this.id
+            && other.quote == this.quote
+
+    override fun hashCode(): Int = Objects.hash(Quote::class.java, id, quote)
+    override fun toString(): String = "Quote{id=$id, quote='$quote'}"
+}
+
+@RestController
+open class QuoteCtrl {
+
+    companion object {
+        val NONE: Quote = Quote(quote = "None")
+        fun nextLong(range: LongRange): Long = range.random(Random.Default)
+    }
+
+    @Autowired
+    lateinit var quoteRepo: QuoteRepo
+
+    @GetMapping("/api")
+    open fun getAll(): List<QuoteResource> = quoteRepo.findAll().map { QuoteResource(it, "success") }
+
+    @GetMapping("/api/{id}")
+    open fun getOne(@PathVariable("id") id: Long?): QuoteResource = quoteRepo
+        .findByIdOrNull(id)
+        ?.let { QuoteResource(it, "success") }
+        ?: QuoteResource(NONE, "Quote $id does not exist")
+
+    @GetMapping("/api/random")
+    open fun getRandomOne(): QuoteResource = getOne(nextLong(1L..quoteRepo.count() + 1))
+}
+
+@Repository
+interface QuoteRepo : JpaRepository<Quote, Long>
+
+data class QuoteResource(val quote: Quote, val type: String)
+
+@SpringBootApplication
+open class QuotersIncorporatedApp {
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(QuotersIncorporatedApp::class.java)
+    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(QuotersIncorporatedApp::class.java, *args)
+}
---
+++ spring-field-app/src/test/kotlin/cfcodefans/study/spring_field/spring/examples/RestServiceTests.kt
@@ -0,0 +1,79 @@
+package cfcodefans.study.spring_field.spring.examples
+
+import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+
+data class Greeting(val id: Long, val content: String)
+
+@RestController
+open class GreetingCtl {
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(GreetingCtl::class.java)
+    }
+
+    val counter: AtomicLong = AtomicLong()
+
+    @GetMapping("/greeting")
+    open fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+        return Greeting(id = counter.incrementAndGet(), "Hello, $name!")
+    }
+}
+
+@SpringBootApplication
+open class GSRestServiceApp {
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(GSRestServiceApp::class.java)
+    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(GSRestServiceApp::class.java, *args)
+}
+
+
+/**
+ * refers to https://spring.io/guides/gs/rest-service/
+ * https://github.com/spring-guides/gs-rest-service/blob/main/complete/src/test/java/com/example/restservice/GreetingControllerTests.java
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+open class RestServiceTests {
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(RestServiceTests::class.java)
+    }
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun noParamGreetingShouldReturnDefaultMessage() {
+        mockMvc.perform(get("/greeting"))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.content").value("Hello, World!"))
+    }
+
+    @Test
+    fun paramGreetingShouldReturnTailoredMessage() {
+        mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.content").value("Hello, World!"))
+    }
+}
